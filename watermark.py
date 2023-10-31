8f1ad80fa45915a7370fb4b5d48834e44bd25d02
hugo <hugo_tso@hotmail.com>
2023-10-31 21:38:12 +0800

    add watermark2

--- watermark.py
+++ watermark.py
@@ -4,7 +4,7 @@ import numpy as np
 
 img = cv2.imread('cover.jpg')
 img = img.astype(np.float16)
-watermark_img = cv2.imread('watermark.jpg')
+watermark_img = cv2.imread('watermark2.jpg')
 watermark_pos = [800, 500]
 img[watermark_pos[0]:watermark_pos[0]+watermark_img.shape[0], watermark_pos[1]:watermark_pos[1]+watermark_img.shape[1], :] += watermark_img
 index = img >= 255
